76c41072669f5830b1458fd0afd62b3d9e8b3d4f
lystalin <lystalin@utexas.edu>
2026-04-23 23:56:19 -0500

    consolidate to one page and update info

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,18 +1,13 @@
-import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/home';
-import AboutMe from './components/about';
-import Navbar from './components/navbar';
 import './App.css';
 
 function App() {
   return (
     <div id = 'whole'>
       <Router>
-        <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} exact />
-          <Route path = '/about/' element = {<AboutMe />} exact />
         </Routes>
       </Router>
     </div>
--- portfolio/src/components/home.js
+++ portfolio/src/components/home.js
@@ -1,28 +1,71 @@
-import React from 'react';
-import './home.css';
-import Headshotcopy from '../images/Headshotcopy.jpg';
-import ResumeFall2024 from './ResumeFall2024.pdf'
-import headshotCircle from '../images/headshotCircle.png'
+import React from "react";
+import "./home.css";
+import { FaEnvelope, FaLinkedin, FaGithub } from "react-icons/fa";
+import HS2Circle from "../images/HS2Circle.png";
+import Resume2026 from "./Resume2026.pdf";
 
-function HomePage() {
-
+function AboutMe() {
   return (
-    <div id = 'body'>
-      <div className = 'header'>
-        <br></br>
-        <img 
-            src={headshotCircle} 
-            alt="Jazmin Reyna Headshot Circle" 
-            id = "headshotCircle"
-        />
-        <div id = 'name'>
-          <b>Hi, I'm Jazmin Reyna</b>
+    <div className = "container-fluid" id = "body">
+      <div className = "row">
+      <div className="col-4 text-center">
+
+      <img 
+        src={HS2Circle} 
+        alt="About me photo" 
+        id="aboutMeImg"
+      />
+
+      <div className="icon-links">
+        <a href="mailto:jazminreyna.atx@gmail.com"><FaEnvelope /></a> 
+        <a href="http://www.linkedin.com/in/jazmin-reyna"><FaLinkedin /></a> 
+        <a href="https://github.com/lystalin"><FaGithub /></a>
+      </div>
+
+      <div className="profile-actions">
+        <a href={Resume2026} target="_blank" rel="noreferrer" className="resume-btn">
+          View Resumé
+        </a>
+      </div>
+
+      </div>
+        <div className="col-8" id = "content">
+          <div id = "my-headers">
+            About Me
+          </div>
+          <div className="about-me">
+            <p>
+              I am a software engineer based in Austin, Texas, and a recent graduate of the University of Texas at Austin, where I earned a degree in Mathematics with a certificate in Elements of Computing. My academic background has provided me with a strong foundation in quantitative analysis and technical problem-solving, allowing me to approach complex challenges with data-driven and software-based solutions.
+            </p>
+
+            <p>
+              I have professional experience working on backend development, data processing pipelines, and engineering tools used to support data workflows and application functionality. In my current role, I have gained exposure to both Python and C++ development in production environments.
+            </p>
+
+            <p>
+              I am proficient in a wide range of tools and technologies, including Python, C++, SQL, and JavaScript, as well as data analysis tools such as Pandas. I also have experience with PostgreSQL, Docker, Git, Linux, CI/CD workflows, and web development frameworks including React.js and Flask, along with data visualization tools such as Tableau, matplotlib, and Plotly.
+            </p>
+
+            <p>
+              As I continue to grow in my career, I am excited about opportunities to work on impactful engineering problems, expand my technical expertise, and contribute to building reliable and efficient software systems.
+            </p>
+          </div>
+          <div id = "my-headers">
+            Work Samples
+          </div>
+          <h4>
+            <a href="https://gitlab.com/michael0403/cs331e-idb">Software Engineering Project (4/2024)</a>
+          </h4>
+          <h4>
+            <a href="https://github.com/lystalin/CS-329E">Data Analysis using Jupyter Notebook</a>
+          </h4>
+          <h4>
+            <a href="https://github.com/lystalin/CS-313E">Assignments using a variety of algorithms</a>
+          </h4>
         </div>
-        <h2>Mathematics student eager to make data driven decisions!</h2>
       </div>
     </div>
   );
 }
 
-export default HomePage;
-
+export default AboutMe;
